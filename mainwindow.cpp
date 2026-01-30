bb1e48eeadf5cdc016c3c4447ad3d2ff79518809
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-01-30 16:53:06 +0300

    feat: подключил MyModel к QTableView в MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_model = new MyModel(this);
+    ui->tableView->setModel(m_model);
+    m_model->insertRows(0, 3); // добавим 3 прямоугольника
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "mymodel.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    MyModel* m_model = nullptr;
 };
 #endif // MAINWINDOW_H
